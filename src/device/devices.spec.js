c8e80989a655f092457fe3c157ad56cdd7154e67
Aaron Jackson <ajackson@pobox.com>
2021-06-26 12:08:26 -0500

    Adding device lambda methods.

---
+++ src/device/devices.spec.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const uuid = require('uuid').v4;
+const expect = require('chai').expect;
+const devices = require('./devices');
+
+let deviceId = uuid();
+let timestamp = (new Date()).toISOString();
+let sourceIP = '192.168.1.1';
+
+describe( 'valid devices', function() {
+	it('should be stored in database', async function () {
+		let myPutItem = jest.fn((parameters, callback) => {
+			callback(undefined, {});
+		});
+		let devicesDAO = new devices.DeviceDAO({
+			dynamodb: {
+				putItem: myPutItem
+			}
+		});
+		let device = {
+			id: deviceId,
+			timestamp: timestamp,
+			source_ip: sourceIP
+		};
+
+		await devicesDAO.put(device);
+		expect(myPutItem.mock.calls.length).is.eq(1);
+	});
+});
