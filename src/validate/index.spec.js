2cf3973e500c5291314ba4f090482d7c8e48babf
Aaron Jackson <ajackson@pobox.com>
2021-07-11 22:48:45 -0500

    Normalizing the model for scoring. Fixing the jest tests.

--- src/validate/index.spec.js
+++ src/validate/index.spec.js
@@ -6,65 +6,34 @@ const lambdaTester = require('lambda-tester');
 const lambdaEventMock = require('lambda-event-mock');
 
 const myLambda = require('./index');
+const validator = require('./base_validator')
+const testData = require('./test-data');
 
-describe( 'Getting a validate', function() {
-	it('returns a value when exists', async function () {
-		let validateId = uuid.v4();
-		let validate = {
-			id: validateId,
-			timestamp: (new Date()).toISOString(),
-			source_ip: undefined
-		};
-
-		let event = lambdaEventMock.apiGateway()
-			.path('/validate')
-			.pathParameters({ id : validateId })
-			.method('GET')
-			.build();
-
-		await lambdaTester(myLambda.handler)
-			.event(event)
-			.expectResult((result) => {
-				expect(result.statusCode).to.eq(200);
-				expect(result.body).is.a('string');
-
-				let value = JSON.parse(result.body);
-				expect(value.id).to.eq(validateId);
-			});
-	});
-});
-
-
-describe( 'Storing a validate', function() {
-	it('should succeed when valid', async function () {
-		let sourceIP = '127.0.0.1'; // default ip (for now)
-		let validate = {
-			source_ip: sourceIP
-		};
-
-		let event = lambdaEventMock.apiGateway()
-			.path('/validate')
-			.method('POST')
-			.body(JSON.stringify(validate))
-			.build();
-
-		let putFunc = jest.fn(value => uuid.v4())
-
-		validates.Singleton.setInstance({
-			put: putFunc
+describe( 'Performing a validation', function() {
+	it('requires a valid infraction', async function () {
+		let validateFunction = jest.fn(async (infraction) => {});
+		validator.Singleton.setInstance({
+			validate: validateFunction
 		});
 
+		let event = lambdaEventMock.sqs()
+			.messageId(uuid.v4())
+			.receiptHandle("receipt-handle")
+			.eventSourceARN(testData.sampleTopicArn)
+			.body(testData.sampleBody)
+			.build();
+
 		await lambdaTester(myLambda.handler)
 			.event(event)
 			.expectResult((result) => {
-				expect(result.statusCode).to.equal(201);
-
-				expect(result.body).is.a('string');
-				expect(result.body).has.length(38);
-
-				expect(putFunc.mock.calls.length).to.be.equal(1);
-				let firstCall = putFunc.mock.calls[0][0];
-				expect(firstCall.source_ip).is.eq(sourceIP);
+				// validator should be called at least once
+				expect(validateFunction.mock.calls.length).to.eq(1);
+				// input to the validator should be a single infraction that matches the sample data
+				let mockArguments = validateFunction.mock.calls[0];
+				expect(mockArguments.length).to.eq(1);
+				let mockInfraction = mockArguments[0];
+				expect(mockInfraction.id).to.eq(testData.sampleInfractionId);
+				expect(mockInfraction.reporter).to.eq(testData.sampleInfractionReporter);
 			});
 	});
 });
